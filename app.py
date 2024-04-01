f79a0fb7a73376138e5d07d0db7d235a6e86d68b
allan <allan.de.castro@escola.pr.gov.br>
2024-04-01 11:46:52 -0300

    aula 2 python2

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ print("2- listar Restaurantes");
 print("3- atiar Restaurante");
 print("4- sair\n");
 
-opcao_digitada = int(input("Escolha um numero de 1 a 4; "))
+opcao_digitada = int(input("Escolha um numero de 1 a 4; "));
 
 print("Você escolheu: ", opcao_digitada);
