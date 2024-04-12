b6635967df3ff7db2fcf2656edef370beeb3c558
Desenvolvimento Sistemas <allan.de.castro@escola.pr.gov.br>
2024-04-12 12:07:34 -0300

    Adicionando restaurantes e adicionando erro para input

--- app.py
+++ app.py
@@ -1,36 +1,60 @@
 import os
 
-def nome():
-    print("Restaurante expresso");
+def finalizar_app():
+    os.system('clear')
+    print('finalizando app\n')
 
+def chamar_nome_do_app():
+    print('restaurante expresso\n')
 
+def escolher_opcoes():
+    print('1- cadastrar restaurante')
+    print('2- listar restaurantes')
+    print('3- ativar restaurante')
+    print('4- sair do programa\n')
 
-def listar():
-    print("1- Cadastrar Restaurante");
-    print("2- listar Restaurantes");
-    print("3- ativar Restaurante");
-    print("4- sair\n");
+def cadastrar_restaurante():
+    nome = input('Digite o nome do restaurante: ')
+    localizacao = input('Digite a localização do restaurante: ')
 
+    restaurantes.append({'nome': nome, 'localizacao': localizacao})
 
-def opcoes():
-    
-    opcao_digitada = int(input("Escolha um numero de 1 a 4; "));
-    print("Você escolheu: ", opcao_digitada);
-    if opcao_digitada == 1:
-        print("Você escolheu 1- Cadastrar Restaurante\n")
-    elif opcao_digitada ==2:
-        print("Você escolheu 2- listar Restaurantes\n")
-    elif opcao_digitada == 3:
-        print("Você escolheu 3- ativar Restaurante\n")
-    else:
-        print("Você escolheu sair\n")
-        os.system('cls')
-        print('Fechando Aplicativo')
+def listar_restaurantes():
+    for restaurante in restaurantes:
+        print(f'Nome: {restaurante["nome"]}, Localização: {restaurante["localizacao"]}')
 
-def main():
-    print("Restaurante expresso");
+def ativar_restaurante():
+    nome = input('Digite o nome do restaurante a ativar: ')
 
-if __name__ == "__main__":
-    main()
-    listar()
-    opcoes()
+    for restaurante in restaurantes:
+        if restaurante['nome'] == nome:
+            restaurante['ativo'] = True
+            print(f'Restaurante {nome} ativado com sucesso!')
+            break
+
+restaurantes = []
+
+while True:
+    finalizar_app()
+    chamar_nome_do_app()
+    escolher_opcoes()
+
+    try:
+        opcao_digitada = int(input('Escolha uma opção: '))
+        print('Você selecionou:', opcao_digitada, '\n')
+
+        if opcao_digitada == 1:
+            cadastrar_restaurante()
+        elif opcao_digitada == 2:
+            listar_restaurantes()
+        elif opcao_digitada == 3:
+            ativar_restaurante()
+        elif opcao_digitada == 4:
+            print('Você escolheu sair do programa\n')
+            break
+        else:
+            print('Opção inválida\n')
+
+    except ValueError:
+        print('Por favor, digite um número válido\n')
+        
